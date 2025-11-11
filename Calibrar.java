35289fcfe5704278155450ba11e814754987a64e
Sunroe-1 <aaaaarsunroe@gmail.com>
2025-11-11 16:34:55 -0400

    Commit #2: Teste Commit Rápido

--- Calibrar.java
+++ Calibrar.java
@@ -1,5 +1,5 @@
 package org.firstinspires.ftc.teamcode;
-
+//Alteracoes
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
